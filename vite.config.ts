f5590aa061cae44d7bd72e5a03677359731f4787
Berat MEN <beratmen9@gmail.com>
2024-12-29 15:10:35 +0300

    Fix static site configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/beratmen/',
+  base: './',
   server: {
     host: '0.0.0.0',
     port: 8080
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: true,
+    emptyOutDir: true
   }
 }) 
